337258dab21ac5029d51e1b719bae19651d939dd
FoxSpellCaster <foxspellcaster@gmail.com>
2024-08-18 21:31:13 -0400

    Main Menu

---
+++ main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/maps/map_lobby.tscn")
+
+
+func _on_settings_pressed() -> void:
+	print("Settings Pressed")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
