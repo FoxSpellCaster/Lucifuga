634c51e2a1a98bf89fa26b947a9c6e11c46cb595
FoxSpellCaster <foxspellcaster@gmail.com>
2024-08-18 22:21:27 -0400

    Clean Up

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/maps/map_lobby.tscn")
+	get_tree().change_scene_to_file("res://levels/level_001.tscn")
 
 
 func _on_settings_pressed() -> void:
