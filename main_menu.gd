1e28f606fdba623ca778a14eb42c7387529a124f
FoxSpellCaster <foxspellcaster@gmail.com>
2024-08-21 23:55:47 -0400

    Clean Up Before Bed

--- main_menu.gd
+++ main_menu.gd
@@ -7,8 +7,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#@warning_ignore("unused_parameter")
+#func _process(delta: float) -> void:
+#	pass
 
 
 func _on_start_pressed() -> void:
