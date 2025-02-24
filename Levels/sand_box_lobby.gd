94c7650cef14b5db8f3d78db11e3851f42b39525
FoxSpellCaster <foxspellcaster@gmail.com>
2025-02-23 19:52:26 -0500

    level design stuff

--- Levels/sand_box_lobby.gd
+++ Levels/sand_box_lobby.gd
@@ -5,8 +5,8 @@ extends Node
 #IDEA How to follow a path in Godot with a camera https://www.youtube.com/watch?v=N1lXsRgmVrg
 #IDEA PrintERR for handy print errors
 #IDEA Time Saving Tips in the Editor https://youtube.com/shorts/W7VrZzk_--Y?si=oIgWjqJUj_wUAd-1
-#IDEA Ghost or Demo Recording. Especially for debugging
-
+#IDEA Ghost or Demo Recording. Especially for debugging https://www.youtube.com/watch?v=Aiu2qIQ2VbU
+#IDEA Model corner walls pillars and create an arch out of them + crate + torch https://www.youtube.com/watch?v=hdyBgQ77Sdg
 
 func _ready():
 	pass
