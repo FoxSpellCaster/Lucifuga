22c7770c4a5051473714713dfcf16318f25e7641
FoxSpellCaster <foxspellcaster@gmail.com>
2025-04-20 15:02:34 -0400

    Quit Crashing Issue
    Video Driver Issue?

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,9 +4,6 @@ func _ready():
 	%Play.pressed.connect(play)
 	%Quit.pressed.connect(quit_game)
 
-#TODO Settings Menu needs to me created and a reverse mouse ignore needs to be added into the node
-#BUG: Error calling from signal 'pressed' to callable: 'CenterContainer::set_mouse_filter': Method expected 1 arguments, but called with 0.
-
 func play():
 	get_tree().change_scene_to_file("res://scenes/LoadingScreen.tscn")
 	
