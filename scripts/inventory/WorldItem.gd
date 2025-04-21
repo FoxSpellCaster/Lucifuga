99b91f4bdee403a0bf85339b841f3497fde73cde
FoxSpellCaster <foxspellcaster@gmail.com>
2025-04-21 12:28:01 -0400

    More Cleanup

--- scripts/inventory/WorldItem.gd
+++ scripts/inventory/WorldItem.gd
@@ -3,6 +3,6 @@ extends InteractableObject
 @export var item_name : String
 
 func _interact ():
-	var item = load("res://Items/ItemData/" + item_name + ".tres")
+	var item = load("res://scenes/inventory/data/" + item_name + ".tres")
 	GlobalSignals.on_give_player_item.emit(item, 1 )
 	queue_free()
