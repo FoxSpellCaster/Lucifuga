6ebb214908b1242799fd32b4269c45db34f62f78
FoxSpellCaster <foxspellcaster@gmail.com>
2025-05-31 15:21:05 -0400

    Steam is not Storing Stats

--- scripts/steam/steam_controller.gd
+++ scripts/steam/steam_controller.gd
@@ -6,6 +6,11 @@ var achievements : Dictionary = {
 	"STAT_FULL_BATTERY": false
 }
 
+var stats : Dictionary = {
+	"CollectedFragments": 0,
+	"ShellsLost": 0
+}
+
 func _init():
 	#var response : Dictionary = Steam.steamInitEx(3673440, true)
 	var response : Dictionary = Steam.steamInitEx(3673440, true)
@@ -32,6 +37,11 @@ func _user_stats_recived(game_id : int, result : int, user_id : int):
 	#reset_archievements()
 	print("Steam Archievements")
 	print(achievements)
+	
+	print("Steam Stats")
+	_load_stats()
+	#set_stat("CollectedFragments", 500)
+	print(stats)
 
 func _load_achievements ():
 	for a in achievements.keys():
@@ -50,3 +60,17 @@ func set_achievement (achievement : String):
 func reset_archievements ():
 	for a in achievements.keys():
 		Steam.clearAchievement(a)
+
+func _load_stats ():
+	for stat in stats.keys():
+		var steam_stat : int = Steam.getStatInt(stat)
+		stats[stat] = steam_stat
+
+func set_stat (stat_name : String, value : int):
+	stats[stat_name] = value
+	
+	if not Steam.setStatInt(stat_name, value):
+		print("Failed to set stat %s to: %s" % [stat_name, value])
+	
+	if not Steam.storeStats():
+		print("Failed to store data on Steam.")
